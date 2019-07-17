87f860425c6afc7d6942929904abbe6d73a723da
unknown <luexe@yandex.ru>
2019-07-17 17:19:16 +0300

    Updates

---
+++ precache-manifest.4dfb97ef689e353346fead0c8463ff77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-app-qr/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "de2b8a4e389a5a4d60c8",
+    "url": "/vk-app-qr/static/js/runtime~main.de2b8a4e.js"
+  },
+  {
+    "revision": "22467253fcb7aa3802be",
+    "url": "/vk-app-qr/static/js/main.22467253.chunk.js"
+  },
+  {
+    "revision": "888f77db16fbad25e8a2",
+    "url": "/vk-app-qr/static/js/2.888f77db.chunk.js"
+  },
+  {
+    "revision": "22467253fcb7aa3802be",
+    "url": "/vk-app-qr/static/css/main.16bd5b9c.chunk.css"
+  },
+  {
+    "revision": "888f77db16fbad25e8a2",
+    "url": "/vk-app-qr/static/css/2.a2d0a773.chunk.css"
+  },
+  {
+    "revision": "777ef135730942da2dad8632008c2d61",
+    "url": "/vk-app-qr/index.html"
+  }
+];
